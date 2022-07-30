5caee482d01f68956ada5305b8757a6cb27339c4
Anand <anandbaburajan@gmail.com>
2022-07-30 20:39:31 +0530

    hash-object and cat-file

--- ugit/data.py
+++ ugit/data.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 
@@ -6,3 +7,16 @@ UGIT_DIR = '.ugit'
 
 def init():
     os.makedirs(UGIT_DIR)
+    os.makedirs(f'{UGIT_DIR}/objects')
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{UGIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
+
+
+def get_object(oid):
+    with open(f'{UGIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
