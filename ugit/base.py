079d131eda373ff77a6a238358468a5913c048eb
Anand <anandbaburajan@gmail.com>
2022-08-02 19:45:03 +0530

    Till write-tree-list-files

---
+++ ugit/base.py
@@ -0,0 +1,18 @@
+from asyncore import write
+import os
+
+from . import data
+
+
+def write_tree(directory='.'):
+    with os.scandir(directory) as it:
+        for entry in it:
+            full = f'{directory}/{entry.name}'
+
+            if entry.is_file(follow_symlinks=False):
+                # TODO write the file to object store
+                print(full)
+            elif entry.is_dir(follow_symlinks=False):
+                write_tree(full)
+
+    # TODO actually create the tree object
