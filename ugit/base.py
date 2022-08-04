abe712698b990d9d33ebb049650168c49c9d92a4
Anand <anandbaburajan@gmail.com>
2022-08-04 17:25:56 +0530

    write-tree-create-tree-obj

--- ugit/base.py
+++ ugit/base.py
@@ -5,14 +5,32 @@ from . import data
 
 
 def write_tree(directory='.'):
+    entries = []
+
     with os.scandir(directory) as it:
         for entry in it:
-            full = f'{directory}/{entry.name}'
+            full_path = f'{directory}/{entry.name}'
+            oid = ''
+
+            if is_ignored(full_path):
+                continue
 
             if entry.is_file(follow_symlinks=False):
-                # TODO write the file to object store
-                print(full)
+                type_ = 'blob'
+                with open(full_path, 'rb') as f:
+                    oid = data.hash_object(f.read())
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
+                type_ = 'tree'
+                oid = write_tree(full_path)
 
-    # TODO actually create the tree object
+            entries.append((type_, oid, entry.name))
+
+    tree = ''.join(f'{type_} {oid} {name}\n'
+                   for type_, oid, name
+                   in sorted(entries))
+
+    return data.hash_object(tree.encode(), 'tree')
+
+
+def is_ignored(path):
+    return '.ugit' in path.split('/')
